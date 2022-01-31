39d95e8584a8cbf559b7eb303ccfbd7ec26c03d2
evtku <evtku@DESKTOP-75JDC4V>
2022-01-31 09:36:03 +0700

    Добавьте файлы проекта.

---
+++ SnakeOOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary;
+
+namespace SnakeOOP 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        { 
+        
+           
+        }
+    
+    
+    }
+
+}
